aad8e5dc474655a14f350f6b0fed2b3489e33f4b
thucnguyen1234 <hantruong3009@gmail.com>
2025-06-08 17:53:39 +0700

    fix síu

--- StreamCartMicroservices/StreamCartMicroservices/src/ApiGateway/ApiGateway/Program.cs
+++ StreamCartMicroservices/StreamCartMicroservices/src/ApiGateway/ApiGateway/Program.cs
@@ -79,7 +79,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
+   // app.UseSwagger();
 }
 
 //app.UseHttpsRedirection();
