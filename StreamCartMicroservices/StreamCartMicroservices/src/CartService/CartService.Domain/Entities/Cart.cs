fb6c7f6bf9b4410958ed1a01ab8f671012844b65
ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
2025-06-15 20:42:13 +0700

    Init CartService

---
+++ StreamCartMicroservices/StreamCartMicroservices/src/CartService/CartService.Domain/Entities/Cart.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Common.Domain.Bases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartService.Domain.Entities
+{
+    public class Cart : BaseEntity
+    {
+        [Required]
+        [ForeignKey("Account")]
+        public Guid CustomerId { get; private set; }
+
+    }
+}
