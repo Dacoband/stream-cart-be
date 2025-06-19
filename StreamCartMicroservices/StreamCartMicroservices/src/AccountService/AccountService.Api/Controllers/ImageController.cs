ff9dd4bb23fce2a774ac22b6f6514c3ac171458a
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-06-19 17:58:55 +0700

    fix route for image

--- StreamCartMicroservices/StreamCartMicroservices/src/AccountService/AccountService.Api/Controllers/ImageController.cs
+++ StreamCartMicroservices/StreamCartMicroservices/src/AccountService/AccountService.Api/Controllers/ImageController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AccountService.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/image")]
     [ApiController]
     public class ImageController : ControllerBase
     {
