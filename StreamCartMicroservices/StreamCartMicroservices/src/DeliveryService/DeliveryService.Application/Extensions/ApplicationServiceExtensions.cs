857abcba5277d32add6e10ba89dd173e2471ff86
ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
2025-06-25 15:22:10 +0700

    [Shipping] Delevery Address

---
+++ StreamCartMicroservices/StreamCartMicroservices/src/DeliveryService/DeliveryService.Application/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using DeliveryService.Application.Interfaces;
+using DeliveryService.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryService.Application.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //Register MediatR
+            //services.AddMediatR(config =>
+            //{
+            //    config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            //});
+
+            // Register services
+            services.AddScoped<IDeliveryAddressInterface, DeliveryAddressService>();
+
+            
+
+            return services;
+        }
+    }
+
+}
---
+++ StreamCartMicroservices/StreamCartMicroservices/src/DeliveryService/DeliveryService.Infrastucture/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DeliveryService.Infrastucture
+{
+    public class Class1
+    {
+
+    }
+}
