d18a66102676ffeedb19d4260cc6dedb465f1976
thucnguyen1234 <hantruong3009@gmail.com>
2025-07-04 18:19:41 +0700

    update database product

---
+++ StreamCartMicroservices/StreamCartMicroservices/src/ProductService/ProductService.Infrastructure/Migrations/20250704111830_SyncFlashSaleSchemaWithDb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncFlashSaleSchemaWithDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndTime",
+                table: "products",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "products",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "products");
+        }
+    }
+}
