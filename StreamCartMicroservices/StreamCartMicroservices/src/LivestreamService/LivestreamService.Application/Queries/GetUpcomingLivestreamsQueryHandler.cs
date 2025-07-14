2ddcaf4f55c368d2fae01cd3758564d5d4509c08
thucnguyen1234 <hantruong3009@gmail.com>
2025-07-15 00:23:04 +0700

    fix

--- StreamCartMicroservices/StreamCartMicroservices/src/LivestreamService/LivestreamService.Application/Queries/GetUpcomingLivestreamsQueryHandler.cs
+++ StreamCartMicroservices/StreamCartMicroservices/src/LivestreamService/LivestreamService.Application/Queries/GetUpcomingLivestreamsQueryHandler.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace LivestreamService.Application.Handlers
+namespace LivestreamService.Application.Queries
 {
     public class GetUpcomingLivestreamsQueryHandler : IRequestHandler<GetUpcomingLivestreamsQuery, List<LivestreamDTO>>
     {
