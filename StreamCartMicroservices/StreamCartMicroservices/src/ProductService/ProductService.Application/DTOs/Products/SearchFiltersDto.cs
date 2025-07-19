b5d6ea34e32d43064e742aa97ec96321536168db
thucnguyen1234 <hantruong3009@gmail.com>
2025-07-19 20:36:32 +0700

    fix run fail

---
+++ StreamCartMicroservices/StreamCartMicroservices/src/ProductService/ProductService.Application/DTOs/Products/SearchFiltersDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductService.Application.DTOs.Products
+{
+    public class SearchFiltersDto
+    {
+        public Guid? CategoryId { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public Guid? ShopId { get; set; }
+        public string SortBy { get; set; } = string.Empty;
+        public bool InStockOnly { get; set; }
+        public int? MinRating { get; set; }
+        public bool OnSaleOnly { get; set; }
+    }
+}
