9d83a8ca4f55f9254b1e222ef5c7552417eab297
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-07-22 15:31:09 +0700

    refresh token: fix
    And update database

---
+++ StreamCartMicroservices/StreamCartMicroservices/src/AccountService/AccountService.Infrastructure/Migrations/20250722082521_AddRefreshTokenFields.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "refresh_token",
+                table: "accounts",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "refresh_token_expiry",
+                table: "accounts",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "refresh_token",
+                table: "accounts");
+
+            migrationBuilder.DropColumn(
+                name: "refresh_token_expiry",
+                table: "accounts");
+        }
+    }
+}
