bd9d6270d12e860f88235c88afc56c7bf04188af
ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
2025-08-01 17:09:27 +0700

    [order] notification queue

---
+++ StreamCartMicroservices/StreamCartMicroservices/src/OrderService/OrderService.Application/Interfaces/IOrderNotificationQueue.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Messaging.Event.OrderEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.Interfaces
+{
+    public interface IOrderNotificationQueue
+    {
+        void Enqueue(OrderCreatedOrUpdatedEvent item);
+        ValueTask<OrderCreatedOrUpdatedEvent> DequeueAsync(CancellationToken cancellationToken);
+    }
+}
