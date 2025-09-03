f870e634eca1a015af233a9a81b4ec2e3ac384a0
thucnguyen1234 <hantruong3009@gmail.com>
2025-09-03 16:15:46 +0700

    fix update

---
+++ StreamCartMicroservices/StreamCartMicroservices/src/ProductService/ProductService.Infrastructure/Migrations/20250903091406_UpdateTablesAndRelations.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTablesAndRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "reserve_stock",
+                table: "products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "reserve_stock",
+                table: "products");
+        }
+    }
+}
