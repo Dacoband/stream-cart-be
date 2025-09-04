c5fc85eb158d3d5f9890cdb0b9b6f694eb02fd1e
thucnguyen1234 <hantruong3009@gmail.com>
2025-09-04 22:51:30 +0700

    update

---
+++ StreamCartMicroservices/StreamCartMicroservices/src/ShopService/ShopService.Infrastructure/Migrations/20250904154903_UpdaeTableRelations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdaeTableRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxLivestream",
+                table: "Shop_memberships",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxLivestream",
+                table: "Shop_memberships");
+        }
+    }
+}
